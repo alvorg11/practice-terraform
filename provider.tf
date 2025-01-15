825b620e9a6de4d7c5c9a10b2c0751f92deb9280
alvorg11 <nagaaaditya73@gmail.com>
2025-01-15 14:29:07 +0530

    updated session-5

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "5.82.2"
     }
   }
+  backend "s3" {
+    bucket = "timing-remot-state"
+    key    = "timing"
+    region = "ap-south-1"
+    dynamodb_table = "timing-lock"
+  }
 }
 
 provider "aws" {
